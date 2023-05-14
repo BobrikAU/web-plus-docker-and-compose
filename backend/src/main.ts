aeba1cad2793635e80d0c93ac43e17244923e6a3
BobrikAU <kirbob0@yandex.ru>
2023-05-14 16:33:24 +0300

    feature: change cors politik

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: true,
+    cors: {
+      origin: [
+        'http://kpd.kirbob.nomoredomains.monster',
+        'https://kpd.kirbob.nomoredomains.monster',
+      ],
+    },
   });
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useGlobalPipes(
